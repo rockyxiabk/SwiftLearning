2788b5e8977c39e580174017a88d700721069329
rockyxiabk <891643419@qq.com>
2018-06-14 09:15:59 +0800

    初始化swift练习仓库

---
+++ First_swift.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var value = 2
+while value<10000{
+    value *= 2
+}
+var i = -8.0
+while i<=8.1 {
+    sin(i)
+    i+=0.1
+}
+
+
